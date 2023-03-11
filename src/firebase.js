0c3c6df1c4f5f9ab01724956c978cfd0eb7c5ebf
Andrii L <andrii.luhvishchyk@fivejars.com>
2023-03-11 09:57:01 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyBxkzEnX9oYHt7m9Vi0zg7dzY_kA8CObgw",
+  authDomain: "team-game-6d99f.firebaseapp.com",
+  projectId: "team-game-6d99f",
+  storageBucket: "team-game-6d99f.appspot.com",
+  messagingSenderId: "890327429423",
+  appId: "1:890327429423:web:f54e548844a962cd6af1da"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
